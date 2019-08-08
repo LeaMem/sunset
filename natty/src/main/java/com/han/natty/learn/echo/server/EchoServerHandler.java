5ac8ddd761b42058f4cee4083c1d42924c1fb8a8
cuijin <lingkou@yscredit.com>
2019-08-08 21:15:42 +0800

    echo server

--- natty/src/main/java/com/han/natty/learn/echo/server/EchoServerHandler.java
+++ natty/src/main/java/com/han/natty/learn/echo/server/EchoServerHandler.java
@@ -11,12 +11,15 @@ import io.netty.util.CharsetUtil;
 @ChannelHandler.Sharable
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
 
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf byteBuf = (ByteBuf) msg;
         System.out.println("Server received: " + byteBuf.toString(CharsetUtil.UTF_8));
         ctx.write(byteBuf);
     }
+
+
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
